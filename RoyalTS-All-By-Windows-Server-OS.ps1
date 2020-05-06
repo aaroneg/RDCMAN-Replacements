1a29a5728a160a54bd3350f5588bff3fe2b36d57
Aaron <git-stuff@aaroneg.com>
2020-05-06 16:19:01 -0500

    fix tabs

--- RoyalTS-All-By-Windows-Server-OS.ps1
+++ RoyalTS-All-By-Windows-Server-OS.ps1
@@ -1,23 +1,23 @@
 import-module RoyalDocument.PowerShell
 $RoyalStore = New-RoyalStore -UserName ($env:USERDOMAIN + '\' + $env:USERNAME)
 $DocumentParams = @{
-    Store    = $RoyalStore
-    Name     = "Windows Servers"
-    FileName = "$PSScriptRoot\windows-servers.rtsz"
+	Store    = $RoyalStore
+	Name     = "Windows Servers"
+	FileName = "$PSScriptRoot\windows-servers.rtsz"
 }
 $RoyalDocument = New-RoyalDocument @DocumentParams
 $Servers = Get-ADComputer -Filter 'OperatingSystem -like "*Server*"' -Properties Name, Description, OperatingSystem, DNSHostname | Select-Object Name, Description, OperatingSystem, DNSHostname | Sort-Object -Property Name
 $OperatingSystems = ($Servers | Select-Object OperatingSystem -Unique).OperatingSystem
 
 ForEach ($OS in $OperatingSystems)  {
-    $folder= New-RoyalObject -Type RoyalFolder -Folder $RoyalDocument -Name $OS -Description "All $OS servers"
-    $folder.CredentialFromParent = $true
-    $FolderItems= $Servers| Where-Object OperatingSystem -EQ $OS
-    Foreach ($Item in $FolderItems) {
-        $RDS = New-RoyalObject -Type RoyalRDSConnection -Folder $folder -Name "$($Item.Name)"
-        $RDS.URI = $Item.DNSHostName
-        $RDS.CredentialFromParent =  $true
-    }
+	$folder= New-RoyalObject -Type RoyalFolder -Folder $RoyalDocument -Name $OS -Description "All $OS servers"
+	$folder.CredentialFromParent = $true
+	$FolderItems= $Servers| Where-Object OperatingSystem -EQ $OS
+	Foreach ($Item in $FolderItems) {
+		$RDS = New-RoyalObject -Type RoyalRDSConnection -Folder $folder -Name "$($Item.Name)"
+		$RDS.URI = $Item.DNSHostName
+		$RDS.CredentialFromParent =  $true
+	}
 }
 
 Out-RoyalDocument -Document $RoyalDocument
